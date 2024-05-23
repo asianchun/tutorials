8ce06186bdb85e9607adcb1583043ed0f2226581
asianchun <hongyuuyi@gmail.com>
2024-05-23 12:07:39 +1000

    added changes

---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Property"
+    
+    name = fields.Char('Property Name')
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From')
+    expected_price = fields.Float("Expected Price")
+    selling_price = fields.Float('Selling Price')
+    bedrooms = fields.Integer('# of Bedrooms')
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer('# of Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
+    garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    
+    
+    
