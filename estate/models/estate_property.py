79dd6e4c6ba710806a110d4684eb3872efe7d073
Nhu Nguyen <nngu0097@student.monash.edu>
2024-05-24 14:03:49 +1000

    Added active and state fields, added default values to some fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import timedelta
 
 
 class Property(models.Model):
@@ -8,16 +9,18 @@ class Property(models.Model):
     name = fields.Char(string='Property Name', required=True)
     description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available From')
+    date_availability = fields.Date('Available From', copy=False, default=fields.Date.today() + timedelta(days=90))
     expected_price = fields.Float(string="Expected Price", required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('# of Bedrooms')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('# of Bedrooms', default=2)
     living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('# of Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(string='Garden Orientation', selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-    
+    active = fields.Boolean(default=True)
+    state = fields.Selection(selection=[('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+                             copy=False, default='new', required=True)
     
     
