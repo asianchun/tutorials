af06b586d5fa57f6c90d1bb55292f8dcd62e2942
asianchun <hongyuuyi@gmail.com>
2024-05-27 13:32:12 +1000

    Conditional button view complete

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -33,6 +33,7 @@ class PropertyOffer(models.Model):
             record.status = 'accepted'
             record.property_id.selling_price = record.price
             record.property_id.buyer_id = record.partner_id
+            record.property_id.state = 'offer accepted'
                     
         return True
     
